71b6ff593e37b62b486d69a7b6a20a78400ca207
Niktia Bykov <kilops.com@gmail.com>
2023-12-14 15:23:12 +0500

    done

--- Program.cs
+++ Program.cs
@@ -5,8 +5,16 @@ class Program
     static void Main(string[] args)
     {
         var input = Console.ReadLine();
-
         if (input == null) return;
+
+        var validationResult = ValidateInput(input);
+
+        if (validationResult.Count > 0)
+        {
+            Console.WriteLine("invalid symbols found in input: {0}", String.Join("", validationResult));
+			return;
+        }
+
         string? result = null;
 
         if (input.Length % 2 == 0)
@@ -29,6 +37,20 @@ class Program
         var charArray = input.ToCharArray();
         Array.Reverse(charArray);
         return new String(charArray);
+    }
+
+    static List<Char> ValidateInput(string input)
+    {
+        var invalidSymbols = new List<Char>();
+        foreach (var character in input.ToCharArray())
+        {
+            if (!(character >= 'a' && character <= 'z'))
+            {
+                invalidSymbols.Add(character);
+            }
+        }
+
+        return invalidSymbols;
     }
 }
 
